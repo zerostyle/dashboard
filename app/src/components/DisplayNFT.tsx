3a4484926af4d36b9a00002b7ddd8e5a508676f1
Josh <josh@zero.style>
2022-06-10 10:57:01 -1000

    fix: slider

--- app/src/components/DisplayNFT.tsx
+++ app/src/components/DisplayNFT.tsx
@@ -2,56 +2,51 @@ import { Box, Button, Center, Flex, Icon, Image, Spinner, Text } from '@chakra-u
 import React, { useCallback, useState } from 'react'
 import { useAppContext } from '../context/AppContext'
 import { FaChevronRight, FaChevronLeft } from 'react-icons/fa'
-import { motion, AnimatePresence } from 'framer-motion'
-import { wrap } from 'popmotion'
+import { motion } from 'framer-motion'
+
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react'
 
 const MotionImage = motion(Image)
-
-const variants = {
-  enter: (direction: number) => {
-    return {
-      x: direction > 0 ? 1000 : -1000,
-      opacity: 0,
-    }
-  },
-  center: {
-    zIndex: 1,
-    x: 0,
-    opacity: 1,
-  },
-  exit: (direction: number) => {
-    return {
-      zIndex: 0,
-      x: direction < 0 ? 1000 : -1000,
-      opacity: 0,
-    }
-  },
-}
 
 const swipeConfidenceThreshold = 10000
 const swipePower = (offset: number, velocity: number) => {
   return Math.abs(offset) * velocity
 }
 
-export function DisplayNFT() {
+export function DisplayNFT({ initial = 0 }: { initial?: number }) {
   const { collection, loading } = useAppContext()
-  const [[page, direction], setPage] = useState([0, 0])
 
-  const imageIndex = wrap(0, collection?.tokens?.length, page)
+  const [loaded, setLoaded] = useState<boolean>(false)
+  const [index, setIndex] = useState<number>(0)
+  const [sliderRef, slider] = useKeenSlider<HTMLDivElement>({
+    initial,
+    loop: true,
+    slideChanged(slider) {
+      setIndex(slider.track.details.rel)
+    },
+    created() {
+      setLoaded(true)
+    },
+  })
 
-  const token = collection?.tokens[imageIndex]?.token
+  const handlePrev = useCallback(
+    (e) => {
+      e.stopPropagation()
+      if (slider) slider.current?.prev()
+    },
+    [slider],
+  )
 
-  const paginate = (newDirection: number) => {
-    setPage([page + newDirection, newDirection])
-  }
+  const handleNext = useCallback(
+    (e) => {
+      e.stopPropagation()
+      if (slider) slider.current?.next()
+    },
+    [slider],
+  )
 
-  const handleNext = useCallback(() => {
-    paginate(1)
-  }, [paginate])
-
-  const handlePrev = useCallback(() => {
-    paginate(-1)
-  }, [paginate])
+  console.log(collection?.tokens)
 
   return (
     <Center position="relative" w="100%" h="100%">
@@ -60,58 +55,36 @@ export function DisplayNFT() {
           <Spinner size="xl" />
         </Center> */}
 
-        <AnimatePresence initial={false} custom={direction}>
-          {!loading && (
-            <MotionImage
-              pos="relative"
-              key={page}
-              src={token?.image?.url}
-              w="100%"
-              h="100%"
-              objectFit="cover"
-              objectPosition="center"
-              custom={direction}
-              variants={variants}
-              initial="enter"
-              animate="center"
-              exit="exit"
-              transition={{
-                x: { type: 'spring', stiffness: 300, damping: 30 },
-                opacity: { duration: 0.2 },
-              }}
-              drag="x"
-              dragConstraints={{ left: 0, right: 0 }}
-              dragElastic={1}
-              onDragEnd={(e, { offset, velocity }) => {
-                const swipe = swipePower(offset.x, velocity.x)
-
-                if (swipe < -swipeConfidenceThreshold) {
-                  paginate(1)
-                } else if (swipe > swipeConfidenceThreshold) {
-                  paginate(-1)
-                }
-              }}
-              style={{
-                mixBlendMode: 'multiply',
-              }}
-            />
-          )}
-        </AnimatePresence>
+        <Flex ref={sliderRef} className="keen-slider">
+          {!!collection?.tokens?.length &&
+            collection.tokens
+              .reverse()
+              .map(({ token }, i) => (
+                <MotionImage
+                  key={i}
+                  className="keen-slider__slide"
+                  pos="relative"
+                  src={token?.image?.url}
+                  w="70vmin"
+                  h="70vmin"
+                  objectFit="cover"
+                  objectPosition="center"
+                />
+              ))}
+        </Flex>
       </Box>
 
-      {!loading && (
-        <Flex pos="absolute" w="100%" bottom={0}>
-          <Flex w="100%" justify="space-between" align="center">
-            <Button onClick={handlePrev}>
-              <Icon as={FaChevronLeft} />
-            </Button>
-            <Text>{token?.name}</Text>
-            <Button onClick={handleNext}>
-              <Icon as={FaChevronRight} />
-            </Button>
-          </Flex>
+      <Flex pos="absolute" w="100%" bottom={0}>
+        <Flex w="100%" justify="space-between" align="center">
+          <Button onClick={handlePrev}>
+            <Icon as={FaChevronLeft} />
+          </Button>
+          {/* <Text>{token?.name}</Text> */}
+          <Button onClick={handleNext}>
+            <Icon as={FaChevronRight} />
+          </Button>
         </Flex>
-      )}
+      </Flex>
     </Center>
   )
 }
