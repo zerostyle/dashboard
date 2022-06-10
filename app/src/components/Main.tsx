7a3caa7c55cd44830266d2ca343c0edddd09169a
Josh <josh@zero.style>
2022-06-10 10:57:30 -1000

    fix: clean up
    # Conflicts:
    #	app/src/components/Main.tsx

--- app/src/components/Main.tsx
+++ app/src/components/Main.tsx
@@ -1,13 +1,17 @@
-import { Stack, StackProps } from '@chakra-ui/react'
+import { Grid, GridItem, Stack, StackProps } from '@chakra-ui/react'
 
-export const Main = (props: StackProps) => (
-  <Stack
-    spacing="1.5rem"
-    width="100%"
-    maxWidth="48rem"
-    mt="-45vh"
-    pt="8rem"
-    px="1rem"
-    {...props}
-  />
+const HEIGHT = 300
+
+export const Main = (props: StackProps) => <Stack spacing="1.5rem" width="100%" maxWidth="48rem" {...props} />
+  <Stack width="100%" maxWidth={1400} px={4} gap={10} {...props}>
+    <Grid templateRows="repeat(2, 1fr)" templateColumns="repeat(12, 1fr)" gap={4}>
+      <GridItem h={HEIGHT} colSpan={3} bg="tomato" />
+      <GridItem h={HEIGHT} colSpan={9} bg="papayawhip" />
+      <GridItem h={HEIGHT} colSpan={6} bg="papayawhip" />
+      <GridItem h={HEIGHT} colSpan={6} bg="papayawhip" />
+      <GridItem h={HEIGHT} colSpan={8} bg="papayawhip" />
+      <GridItem h={HEIGHT} colSpan={4} bg="purple.900" />
+      <GridItem h={HEIGHT} colSpan={12} bg="papayawhip" />
+    </Grid>
+  </Stack>
 )
