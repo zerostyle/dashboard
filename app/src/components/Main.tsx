0a20cb041ae5ac0529384fddba7e64d35663014c
Josh <josh@zero.style>
2022-06-12 20:33:14 -1000

    fix: loader

--- app/src/components/Main.tsx
+++ app/src/components/Main.tsx
@@ -1,4 +1,4 @@
-import { AspectRatio, Box, Center, Heading, Image } from '@chakra-ui/react'
+import { Box, Center, Heading, Image } from '@chakra-ui/react'
 import { useAppContext } from '../context/AppContext'
 import { DisplayNFT } from './DisplayNFT'
 import { motion } from 'framer-motion'
@@ -31,20 +31,18 @@ export const Main = () => {
             animate={{ opacity: 1 }}
             transition={{ duration: 0.5, delay: 0.2 }}
           >
-            <AspectRatio ratio={1}>
-              <Image
-                src="/loading-skull-noun.gif"
-                pos="relative"
-                w="100%"
-                h="100%"
-                maxW="70vmin"
-                maxH="70vmin"
-                objectFit={['cover', 'cover', 'contain']}
-                objectPosition="center"
-                bg="blackAlpha.500"
-                borderRadius={12}
-              />
-            </AspectRatio>
+            <Image
+              src="/loading-skull-noun.gif"
+              pos="relative"
+              w="100%"
+              h="100%"
+              maxW="70vmin"
+              maxH="70vmin"
+              objectFit={['cover', 'cover', 'contain']}
+              objectPosition="center"
+              bg="blackAlpha.500"
+              borderRadius={12}
+            />
           </MotionCenter>
 
           <Box h={50} />
